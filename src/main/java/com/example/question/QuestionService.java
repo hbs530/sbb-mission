7cfaaf566b7ae391a90f333ffed31f11e8125467
Hyun Beomseok <hbs530@naver.com>
2023-11-25 07:32:46 +0900

    2-10

--- src/main/java/com/example/question/QuestionService.java
+++ src/main/java/com/example/question/QuestionService.java
@@ -1,6 +1,9 @@
 package com.example.question;
 
 import java.util.List;
+import java.util.Optional;
+
+import com.example.sbbmission.DataNotFoundException;
 
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
 
