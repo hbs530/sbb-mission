042abd217b56521973644a305a7e0bfb3ee737ae
Hyun Beomseok <hbs530@naver.com>
2023-11-25 16:50:34 +0900

    3-07

--- src/main/java/com/example/sbbmission/SecurityConfig.java
+++ src/main/java/com/example/sbbmission/SecurityConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,12 +24,23 @@ public class SecurityConfig {
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/"))
         ;
         return http.build();
     }
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
 
--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -48,5 +48,9 @@ public class UserController {
         }
         return "redirect:/";
     }
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
 }
 
