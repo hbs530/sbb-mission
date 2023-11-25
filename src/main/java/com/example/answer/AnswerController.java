7c571abc6ae23c2eec0df672a8776f9948d79365
Hyun Beomseok <hbs530@naver.com>
2023-11-25 19:04:59 +0900

    3-12

--- src/main/java/com/example/answer/AnswerController.java
+++ src/main/java/com/example/answer/AnswerController.java
@@ -38,9 +38,10 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        // TODO: 답변을 저장한다.
-        this.answerService.create(question, answerForm.getContent(siteUser));
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question,
+                answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
@@ -64,8 +65,8 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
-    }
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());    }
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
     public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
@@ -74,7 +75,8 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
         }
         this.answerService.delete(answer);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/vote/{id}")
@@ -82,7 +84,7 @@ public class AnswerController {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
-    }
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());    }
 }
 
--- src/main/java/com/example/answer/AnswerService.java
+++ src/main/java/com/example/answer/AnswerService.java
@@ -15,7 +15,7 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
