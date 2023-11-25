188da014d6858ad0fb9d2b05502e9349b42a2b6f
Hyun Beomseok <hbs530@naver.com>
2023-11-25 15:31:13 +0900

    3-02

--- src/main/java/com/example/question/QuestionController.java
+++ src/main/java/com/example/question/QuestionController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 import com.example.answer.AnswerForm;
@@ -23,9 +26,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(@RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
     @GetMapping(value = "/detail/{id}")
--- src/main/java/com/example/question/QuestionService.java
+++ src/main/java/com/example/question/QuestionService.java
@@ -5,8 +5,13 @@ import java.util.Optional;
 import java.time.LocalDateTime;
 
 import com.example.sbbmission.DataNotFoundException;
-
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +33,14 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
     }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
+
     public void create(String subject, String content) {
         Question q = new Question();
         q.setSubject(subject);
